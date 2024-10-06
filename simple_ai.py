39bf1120e13537b45ee7331eb0cc56730c6bfb3e
Petr Čermák <petr.cermak@matfyz.cuni.cz>
2024-10-06 14:15:51 +0200

    First version of Rambo engine

--- simple_ai.py
+++ simple_ai.py
@@ -5,7 +5,11 @@ import numpy as np
 from supremacy import helpers
 
 # This is your team name
-CREATOR = "SimpleAI"
+CREATOR = "John"
+
+TYPES = {}
+JET_Y_CORDS = {}
+JET_X_CORDS = {}
 
 
 def tank_ai(tank, info, game_map):
@@ -23,20 +27,79 @@ def ship_ai(ship, info, game_map):
     """
     Function to control ships.
     """
-    if not ship.stopped:
-        if ship.stuck:
-            if ship.get_distance(ship.owner.x, ship.owner.y) > 20:
-                ship.convert_to_base()
-            else:
-                ship.set_heading(np.random.random() * 360.0)
-
+    radius = 80
+    if ship.uid not in TYPES:
+         TYPES[ship.uid] = "explorer"
+         print(f"setting SHIP {ship.uid} to explorer")
+    if TYPES[ship.uid] == "explorer":
+        if not ship.stopped:
+            if ship.stuck:
+                # go over bases
+                alone = True
+                for b in info["bases"]:
+                    if ship.get_distance(b.x, b.y) < radius:
+                        alone = False
+                        break
+                if alone:
+                    ship.convert_to_base()
+                else:
+                    ship.set_heading(np.random.random() * 360.0)
+    else:
+        #list all bases:
+        print("UNKNOWN TYPE of the SHIP")
+        pass
+            
 
 def jet_ai(jet, info, game_map):
     """
     Function to control jets.
     """
-    if "target" in info:
-        jet.goto(*info["target"])
+    if jet.uid not in TYPES:
+        # max explorers
+        maxexp = game_map.shape[0] / 20
+        if len(JET_Y_CORDS) < maxexp:
+            ttype = "explorer1"
+        else:
+            ttype = "attacker"            
+        TYPES[jet.uid] = ttype
+        print(f"setting JET {jet.uid} to {ttype}")
+    if TYPES[jet.uid] == "explorer1":
+        # get empty row
+        ypos = -1e6
+        ## arrived?
+        if jet.uid not in JET_Y_CORDS:
+            if len(JET_Y_CORDS) == 0:
+                ypos = jet.owner.y
+            else:
+                for v in JET_Y_CORDS.values():
+                    ypos = max(ypos, v)
+                ypos = ypos + 20
+            JET_Y_CORDS[jet.uid] = ypos
+            jet.goto(jet.owner.x, ypos)
+        else:
+            if abs(JET_Y_CORDS[jet.uid] % game_map.shape[0]-jet.y) < 4:
+                print(f"Explorer1 arrived, next level!")
+                TYPES[jet.uid] = "explorer2"
+    if TYPES[jet.uid] == "explorer2":
+        if jet.uid not in JET_X_CORDS:
+            JET_X_CORDS[jet.uid] = (jet.owner.x + game_map.shape[1] / 2) % game_map.shape[1]
+            #left right?
+            if len(JET_X_CORDS) % 2 == 0:
+                jet.goto(jet.x + 100, JET_Y_CORDS[jet.uid])
+            else:
+                jet.goto(jet.x - 100, JET_Y_CORDS[jet.uid])
+        elif abs(JET_X_CORDS[jet.uid]-jet.x) < 4:
+            print(f"Explorer2 arrived, next level!")
+            TYPES[jet.uid] = "attacker"
+    if TYPES[jet.uid] == "attacker":
+        shortest = 1e9
+        target = [0,0]
+        for b in info["ebases"]:
+            if jet.get_distance(b.x, b.y) < shortest:
+                target = [b.x,b.y]
+                shortest = jet.get_distance(b.x, b.y)
+        jet.goto(*target)
+        
 
 
 class PlayerAi:
@@ -44,11 +107,24 @@ class PlayerAi:
     This is the AI bot that will be instantiated for the competition.
     """
 
+    blevels = {}
+
     def __init__(self):
         self.team = CREATOR  # Mandatory attribute
-        self.build_queue = helpers.BuildQueue(
-            ["mine", "tank", "ship", "jet"], cycle=True
-        )
+
+
+    def build(self, base, kind = "mine"):
+        if base.crystal < base.cost(kind):
+            return None
+        else:
+            kwargs = {}
+            if kind != "mine":
+                kwargs["heading"] = 360 * np.random.random()
+            build = getattr(base, f"build_{kind}")(**kwargs)
+            return True
+
+
+
 
     def run(self, t: float, dt: float, info: dict, game_map: np.ndarray):
         """
@@ -57,12 +133,26 @@ class PlayerAi:
 
         # Get information about my team
         myinfo = info[self.team]
+        myinfo["ebases"] = []
 
         # Iterate through all my bases and process build queue
-        for base in myinfo["bases"]:
-            # Calling the build_queue will return the object that was built by the base.
-            # It will return None if the base did not have enough resources to build.
-            obj = self.build_queue(base)
+        for b in myinfo["bases"]:
+            if b.uid not in self.blevels:
+                lvl = 0
+            else:
+                lvl = self.blevels[b.uid]
+            if lvl < 5:
+                bb = self.build(b, "mine")
+            elif lvl < 12:
+                bb = self.build(b, "ship")
+            else:
+                if lvl % 2 == 0:
+                    bb = self.build(b, "jet")
+                else:
+                    bb = self.build(b, "tank")
+            if bb:
+                print(f"mine done at {lvl=}")
+                self.blevels[b.uid] = lvl+1
 
         # Try to find an enemy target
         # If there are multiple teams in the info, find the first team that is not mine
@@ -72,7 +162,10 @@ class PlayerAi:
                     # Target only bases
                     if "bases" in info[name]:
                         # Simply target the first base
+                        for b in info[name]["bases"]:
+                            myinfo["ebases"].append(b)
                         t = info[name]["bases"][0]
+                        print(f"BASE of {name} targeted!")
                         myinfo["target"] = [t.x, t.y]
                         break
 
